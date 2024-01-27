08c15dfe928e2e2357fa122b9a2be9cfc2e4e1d1
Padmakshi V <padmakshihk@gmail.com>
2024-01-27 15:03:31 +0530

    Create Find a Peak Element II.java

---
+++ Find a Peak Element II.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] findPeakGrid(int[][] G) {
+        int n = G.length;
+        int m = G[0].length;
+
+        int low = 0;
+        int high = m - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int maxRowIndex = findMaxIndex(G, n, m, mid);
+
+            int left = (mid - 1 >= 0) ? G[maxRowIndex][mid - 1] : -1;
+            int right = (mid + 1 < m) ? G[maxRowIndex][mid + 1] : -1;
+
+            if (G[maxRowIndex][mid] > left && G[maxRowIndex][mid] > right) {
+                return new int[]{maxRowIndex, mid};
+            } else if (G[maxRowIndex][mid] < left) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+    public int findMaxIndex(int[][] mat, int n, int m, int col) {
+        int maxValue = -1;
+        int index = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (mat[i][col] > maxValue) {
+                maxValue = mat[i][col];
+                index = i;
+            }
+        }
+
+        return index;
+    }
+}
